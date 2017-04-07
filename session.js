1f0f921b72871831d2edc10ce8937679f51fa9f7
hugongludashi <1301775288@qq.com>
2017-04-07 11:47:23 +0800

    Create session.js

---
+++ session.js
@@ -0,0 +1,28 @@
+//set cookies
+function setCookie(name,value,days){
+  var dage, expires;
+  if (days){
+    date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toGMTString();
+      }else{
+    expires = "";
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+//get cookies
+function getCookie(name){
+	var i, c, ca, nameEQ = name + "=";
+	ca = document.cookie.split(';');
+	for(i=0;i<ca.length;i++){
+		c=ca[i];
+		while (c.charAt(0)==' '){
+			c = c.substring(1,c.length);
+		}
+		if (c.indexOf(nameEQ) == 0){
+			return c.substring(nameEQ.length,c.length);
+		}
+    }
+	return '';
+}
