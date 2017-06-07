b4483e8b5b1086547ebf2b15af19fae1785e54dc
hugongludashi <1301775288@qq.com>
2017-06-07 16:38:18 +0800

    Create factorial.js

---
+++ factorial.js
@@ -0,0 +1,10 @@
+//arguments.callee is a pointer to the function being executed and, as such, can be used
+//to call the function recursively
+
+function factorial(num){
+  if (num<=1){
+    return 1;
+  }else{
+    return num*arguments.callee(num-1);
+  }
+}
